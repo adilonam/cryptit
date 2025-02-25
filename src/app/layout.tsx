db8944961754710e657fd1c2dcb6c7995da1fd6a
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-02-25 10:29:17 +0000

    resolve google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
-"use client";
 import "jsvectormap/dist/css/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/satoshi.css";
 import "@/css/style.css";
-import React, { useEffect, useState } from "react";
-import Loader from "@/components/common/Loader";
-import { Session } from "next-auth";
-import NextAuthProvider from "@/components/providers/NextAuthProvider";
+import React from "react";
 import { Toaster } from 'react-hot-toast';
 import { GoogleTagManager } from '@next/third-parties/google'
 
@@ -15,29 +11,20 @@ type RootLayoutProps = {
 };
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loading, setLoading] = useState<boolean>(true);
 
-  // const pathname = usePathname();
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
 
   return (
     <html lang="en" suppressHydrationWarning>
-  
   <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID as string} />
-   
-     
-      <body suppressHydrationWarning={true}>
+
+  
+      <body >
       
-        {/* <NextAuthProvider session={session}> */}
         <Toaster />
           <div className="dark:bg-boxdark-2 dark:text-bodydark">
-            {loading ? <Loader /> : children}
+            {children}
           </div>
-        {/* </NextAuthProvider> */}
+    
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
+"use client"
 import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React, { useEffect, useState } from "react";
+import Loader from "@/components/common/Loader";
 
-export const metadata: Metadata = {
+ const metadata: Metadata = {
   title:
     "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
   description: "This is Next.js Home for TailAdmin Dashboard Template",
 };
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000); // 5 seconds delay
+  }, []);
+
   return (
-    <>
-      <DefaultLayout>
-        <ECommerce />
-      </DefaultLayout>
-    </>
+ 
+    <DefaultLayout>
+    {loading ? <Loader /> : <ECommerce />}
+    </DefaultLayout>
+     
+ 
   );
 }
